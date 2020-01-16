afd893bb86a15760556d67e8448d2e83ed01e64e
cdagli <caner.dagli@qlik.com>
2020-01-16 13:35:53 +0100

    exit 1

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ const main = async () => {
   }
 };
 
-main().catch(error => {
+main().then(() => {
+  process.exit(1);
+}).catch(error => {
   console.error(error);
-;
+  process.exit(1);
 });
-
-process.exit(1)
