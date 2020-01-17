76e11689f7a400cbdb45b64ccab5c8ca28db9e90
cdagli <caner.dagli@qlik.com>
2020-01-17 08:05:23 +0100

    Table

--- index.js
+++ index.js
@@ -3,63 +3,46 @@ const github = require("@actions/github");
 const util = require("util");
 const path = require("path");
 const readPackageJSON = require("read-package-json");
-const fs = require("fs");
-const exec = require("@actions/exec");
-
-let myOutput = "";
-let myError = "";
-
-const options = {};
-options.listeners = {
-  stdout: data => {
-    myOutput += data.toString();
-  },
-  stderr: data => {
-    myError += data.toString();
-  }
-};
-options.cwd = "./lib";
+const compareVersions = require('compare-versions');
 
 const readJSON = util.promisify(readPackageJSON);
 
 const main = async () => {
   try {
-    // await exec.exec("pwd");
-    // console.log('FooOutput: ', myOutput);
-    // console.log('FooError:' ,myError);
-    // await exec.exec("ls");
-    // await exec.exec("git branch");
-    // await exec.exec("git diff");
-    // console.log('FooOutput: ', myOutput);
-    // console.log('FooError:' ,myError);
-    // `who-to-greet` input defined in action metadata file
-    // const nameToGreet = core.getInput("who-to-greet");
-    // console.log(`Hello ${nameToGreet}!`);
-    // const time = new Date().toTimeString();
-    // core.setOutput("time", time);
-    // // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2);
-    // console.log(`The event payload: ${payload}`);
-
     const workspace = process.env.GITHUB_WORKSPACE;
-    // console.log(workspace)
-    // fs.readdirSync(workspace).forEach(file => {
-    //   console.log('FILE')
-    //   console.log(file);
-    // });
     const project = await readJSON(path.join(workspace, "package.json"));
-    console.log(project.dependencies);
 
     const approved = await readJSON(path.join(workspace, "approved.json"));
+
     const result = true;
-    Object.keys(project.dependencies).forEach(dependency => {
-      if (!approved[dependency]) {
-        console.log(`${dependency} is not approved`);
+    const productApprovalResults = [];
+
+    Object.keys(project.dependencies).forEach((dependency, index) => {
+      if (!approved[dependency] || compareVersions(approved[dependency], project.dependencies[index]) === -1 ) {
+        productApprovalResults.push({
+          name: dependency,
+          version: project.dependencies[index],
+          approvedVersion: '-',
+          status: 'FAILED'
+        })
         result = false;
       }
+
+      productApprovalResults.push({
+        name: dependency,
+        version: project.dependencies[index],
+        approvedVersion: approved[dependency],
+        status: 'SUCCESS'
+      })
     });
 
-    console.log(result)
+    console.table(productApprovalResults);
+
+    if(!result) {
+      throw new Error('Dependency check failed!');
+    }
+
+    console.log('Dependency check passed!')
 
   } catch (error) {
     core.setFailed(error.message);
@@ -67,10 +50,14 @@ const main = async () => {
 };
 
 main()
-  .then(() => {
-    process.exit(1);
-  })
   .catch(error => {
     console.error(error);
     process.exit(1);
   });
+
+
+/*
+- Success
+- Not approved
+- Not approved version 
+*/
